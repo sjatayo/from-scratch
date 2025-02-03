57f405758d00f2670e65d4dd3b8856503efdd08b
sjatayo <sjatayo@hiof.no>
2025-02-03 14:00:37 +0100

    first commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import "../styles/nav.scss";
+
+export default function Nav(){
+return(
+    <header>
+        <nav>
+            <a href="">ToDo's</a>
+            <a href="">brukernavn</a>
+        </nav>
+    </header>
+)
+}
